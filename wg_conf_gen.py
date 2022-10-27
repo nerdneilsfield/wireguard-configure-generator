568e1aaad89a7df92fabf20f890be0c9c4689c36
Dengqi <me@dengqi.org>
2022-10-28 06:45:06 +0800

    feat: export example config

--- wg_conf_gen.py
+++ wg_conf_gen.py
@@ -26,74 +26,123 @@ def gen_key_pair() -> (str, str, str):
     psk = psk_output.decode("utf-8").strip()
     return (key, pubkey, psk)
 
+def gen_example(path):
+    with open(path, "w") as f:
+        f.write("""
+{
+    "clients": [
+        {
+            "dns1": "10.15.44.11",
+            "dns2": "114.114.114.114",
+            "gen_global": true,
+            "gen_local": true,
+            "name": "my-clients",
+            "port": 8080,
+            "vlan_ipv4_addr": "10.0.0.2"
+        }
+    ],
+    "common": {
+        "network_ipv4_addr": "10.0.0.0/24",
+        "network_name": "my-network"
+    },
+    "server": {
+        "interface": "en0",
+        "ipv4_addr": "10.0.0.1",
+        "name": "my-server",
+        "port": 8080,
+        "vlan_ipv4_addr": "114.114.11.11"
+    }
+}
+    """)
 
-def gen_server_config():
-    pass
+
+# def gen_server_config(configs):
+#     pass
+#
+#
+# def gen_client_config(configs):
+#     pass
 
 
-def gen_client_config():
-    pass
 
+if __name__ == "__main__":
 
-configs = json.load(open(sys.argv[1]))
+    check_wg_exists()
+
+    logging.basicConfig(format="%(asctime)s:%(levelname)s: %(message)s", level=logging.INFO)
+
+    arg_parser = argparse.ArgumentParser(description="generate wireguard config files from json file")
+
+    arg_parser.add_argument("-c", "--config", type=str, default="config.json", help="the path to configuration file")
+    arg_parser.add_argument("-o", "--output", type=str, default=".", help="the path to output directory")
+    arg_parser.add_argument("--gen-example", action="store_true", default=False, help="flag to enable generate example")
+    arg_parser.add_argument("--gen-example-path", type=str, default="config.example.json", help="output a example json configuration file")
+
+    args = arg_parser.parse_args()
+
+    if args.gen_example:
+        gen_example(args.gen_example_path)
+        sys.exit(0)
+
+    logging.info(f"loading configuration file {args.config}")
+    configs = json.load(open(args.config))
 # print(configs)
 
 # keys = {}
-if "prvkey" not in configs["server"].keys():
-    server_key_pair = gen_key_pair()
-    configs["server"]["prvkey"] = server_key_pair[0]
-    configs["server"]["pubkey"] = server_key_pair[1]
-    configs["server"]["psk"] = server_key_pair[2]
+    logging.debug("generate keys for server and clients")
+    if "prvkey" not in configs["server"].keys():
+        server_key_pair = gen_key_pair()
+        configs["server"]["prvkey"] = server_key_pair[0]
+        configs["server"]["pubkey"] = server_key_pair[1]
+        configs["server"]["psk"] = server_key_pair[2]
 
-for client in configs["clients"]:
-    if "prvkey" not in client.keys():
-        client_key_pair = gen_key_pair()
-        client["prvkey"] = client_key_pair[0]
-        client["pubkey"] = client_key_pair[1]
-        client["psk"] = client_key_pair[2]
+    for client in configs["clients"]:
+        if "prvkey" not in client.keys():
+            client_key_pair = gen_key_pair()
+            client["prvkey"] = client_key_pair[0]
+            client["pubkey"] = client_key_pair[1]
+            client["psk"] = client_key_pair[2]
 
 # print(configs)
 
 # gen server config
-print("Gen config for server")
-server_config_name = (
-    f"wg-{configs['common']['network_name']}-server-{configs['server']['name']}.conf"
-)
-server_config_path = os.path.join(os.path.abspath("."), server_config_name)
+    logging.info("generate config file for server")
+    server_config_name = (
+        f"wg-{configs['common']['network_name']}-server-{configs['server']['name']}.conf"
+    )
+    server_config_path = os.path.join(os.path.abspath(args.output), server_config_name)
 
-with open(server_config_path, "w") as f:
-    server = configs["server"]
-    common = configs["common"]
-    server_interface = server["interface"]
-    network_name = common["network_name"]
-    f.write(
+    with open(server_config_path, "w") as f:
+        server = configs["server"]
+        common = configs["common"]
+        server_interface = server["interface"]
+        network_name = common["network_name"]
+        f.write(
         f"""[Interface]
-Address = {server["ipv4_addr"]}/24
+Address = {server["vlan_ipv4_addr"]}/24
 ListenPort = {server["port"]}
 PrivateKey = {server["prvkey"]}
 PostUp = iptables -A FORWARD -i {server_interface} -o {network_name} -j ACCEPT; iptables -A FORWARD -i {network_name} -j ACCEPT; iptables -t nat -A POSTROUTING -o {server_interface} -j MASQUERADE
 PostDown = iptables -D FORWARD -i {server_interface} -o {network_name} -j ACCEPT; iptables -D FORWARD -i {network_name} -j ACCEPT; iptables -t nat -D POSTROUTING -o {server_interface} -j MASQUERADE
     """
     )
-    for client in configs["clients"]:
-        f.write(
-            f"""
+        for client in configs["clients"]:
+            f.write(f"""
 ### Client {client["name"]}
 [Peer]
 PublicKey = {client["pubkey"]}
 PresharedKey = {client["psk"]}
-AllowedIPs = {client["ipv4_addr"]}/32
-"""
-        )
+AllowedIPs = {client["vlan_ipv4_addr"]}/32
+""")
 
 # gen client config
-for client in configs["clients"]:
-    print("Gen config for client: ", client["name"])
-    server = configs["server"]
-    common = configs["common"]
-    common_header = f"""[Interface]
+    for client in configs["clients"]:
+        logging.info(f'generate config fie for client {client["name"]}')
+        server = configs["server"]
+        common = configs["common"]
+        common_header = f"""[Interface]
 PrivateKey = {client["prvkey"]}
-Address = {client["ipv4_addr"]}/32
+Address = {client["vlan_ipv4_addr"]}/32
 DNS = {client["dns1"]},{client["dns2"]}
 
 # server {server["name"]}
@@ -103,35 +152,35 @@ PresharedKey = {client["psk"]}
 Endpoint = {server["ipv4_addr"]}:{server["port"]}
 PersistentKeepalive = 25
 """
-    if client["gen_global"]:
-        config_global_name = (
-            f'wg-{common["network_name"]}-client-{client["name"]}-global.conf'
-        )
-        config_global_path = os.path.join(os.path.abspath("."), config_global_name)
+        if client["gen_global"]:
+            config_global_name = (
+                f'wg-{common["network_name"]}-client-{client["name"]}-global.conf'
+            )
+            config_global_path = os.path.join(os.path.abspath(args.output), config_global_name)
 
-        global_configs = (
-            common_header
-            + """AllowedIPs = 0.0.0.0/0
+            global_configs = (
+                common_header
+                + """AllowedIPs = 0.0.0.0/0
 """
-        )
+            )
 
-        with open(config_global_path, "w") as f:
-            f.write(global_configs)
+            with open(config_global_path, "w") as f:
+                f.write(global_configs)
 
-    if client["gen_global"]:
-        config_local_name = (
-            f'wg-{common["network_name"]}-client-{client["name"]}-local.conf'
-        )
-        config_local_path = os.path.join(os.path.abspath("."), config_local_name)
+        if client["gen_local"]:
+            config_local_name = (
+                f'wg-{common["network_name"]}-client-{client["name"]}-local.conf'
+            )
+            config_local_path = os.path.join(os.path.abspath(args.output), config_local_name)
 
-        local_configs = (
-            common_header
-            + f"""AllowedIPs = {common['network_ipv4_addr']}
+            local_configs = (
+                common_header
+                + f"""AllowedIPs = {common['network_ipv4_addr']}
 """
-        )
+            )
 
-        with open(config_local_path, "w") as f:
-            f.write(local_configs)
+            with open(config_local_path, "w") as f:
+                f.write(local_configs)
 
 
-json.dump(configs, open(sys.argv[1], "w"))
+    json.dump(configs, open(args.config, "w"))
